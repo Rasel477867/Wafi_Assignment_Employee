d2684fa4cfe87ede08078e8e0a93d22fae270fa5
“Rasel477867” <raselaye3737@gamil.com>
2024-09-13 10:32:34 +0600

    Repository Pattern and UnitOfWork Completed.

---
+++ EmployeesRepository/Core/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesRepository.Core
+{
+    public interface IRepository
+    {
+        public interface IRepository<T> where T : class
+        {
+            Task<T> GetById(Guid id);
+            Task<List<T>> GetAll();
+            Task Update(T Entity);
+            Task Delete(T Entity);
+            Task  Add(T Entity);
+
+
+        }
+    }
+}
---
+++ EmployeesRepository/Core/Repository.cs
@@ -0,0 +1,69 @@
+﻿using EmployeesCore.EntityModel.Core;
+using EmployeesRepository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EmployeesRepository.Core.IRepository;
+
+namespace EmployeesRepository.Core
+{
+    public abstract class Repository<T> : IRepository<T> where T : Entity
+    {
+        private readonly ApplicationDbContext _db;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public DbSet<T> table
+        {
+            get
+            {
+                return _db.Set<T>();
+            }
+        }
+        public virtual async Task Add(T Entity)
+        {
+
+            await table.AddAsync(Entity);
+           
+        }
+
+        public virtual async Task Delete(T Entity)
+        {
+            await Task.Run(() =>
+            {
+                Entity.IsDeleted = true;
+                table.Update(Entity);
+              
+            });
+      
+
+        }
+
+        public virtual async Task<List<T>> GetAll()
+        {
+            return await table.Where(c => !c.IsDeleted).ToListAsync();
+        }
+
+        public virtual async Task<T> GetById(Guid id)
+        {
+            var query=  table.Where(x=>!x.IsDeleted && x.Id == id).AsQueryable();
+
+            return await query.AsNoTracking().FirstOrDefaultAsync();
+        }
+
+        public virtual async Task Update(T Entity)
+        {
+            await Task.Run(() =>
+            {
+
+                table.Update(Entity);
+
+            });
+           
+        }
+    }
+}
